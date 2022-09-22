cea3e28a865a69b6e49aef42e17c5f768cb975cb
Boris I. Bendovsky <bibendovsky@hotmail.com>
2022-09-22 16:29:22 +0300

    Use SPDX license identifier

--- cmake/FindSDL2W_net.cmake
+++ cmake/FindSDL2W_net.cmake
@@ -1,27 +1,8 @@
 #[[
-
 SDL2W - CMake wrapper for SDL2.
 
 Copyright (c) 2020-2021 Boris I. Bendovsky (bibendovsky@hotmail.com) and Contributors.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
-OR OTHER DEALINGS IN THE SOFTWARE.
-
+SPDX-License-Identifier: MIT
 
 Virtual components:
     - static - uses static version of SDL2_net.
@@ -33,11 +14,10 @@ Required variables:
 
 Targets:
     - SDL2W_net::SDL2W_net - SDL2_net.
-
 ]]
 
 
-cmake_minimum_required (VERSION 3.1.3 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.1.3 FATAL_ERROR)
 
 set(SDL2W_NET_VERSION "1.0.1")
 message(STATUS "[SDL2W_net] Version: ${SDL2W_NET_VERSION}")
